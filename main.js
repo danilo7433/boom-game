669c3ab380f19f8b7ed6b2be6a8bf733414d1944
danilo Lima Dias <danilo743377@gmail.com>
2020-11-02 23:10:33 -0300

    criacao de niveis para o jogo, contagem de balaoe pontuacao

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-const buttonInitGame = document.querySelector(".btn-init-game");
+const buttonBeginner = document.querySelector(".btn-beginner");
+const buttonMedian = document.querySelector(".btn-median");
+const buttonProfessional = document.querySelector(".btn-professional");
+const contadorDeBalao = document.querySelector('.contador')
 
-buttonInitGame.addEventListener("click", function () {
-  buttonInitGame.remove();
-  initGame();
+
+
+const iniciante = buttonBeginner.addEventListener("click", function () {
+  buttonMedian.remove();
+  buttonBeginner.remove();
+  buttonProfessional.remove()
+  beginner();
 });
 
-function initGame() {
+const mediano = buttonMedian.addEventListener("click", function () {
+  buttonMedian.remove();
+  buttonBeginner.remove();
+  buttonProfessional.remove()
+  median();
+});
+
+const profissional = buttonProfessional.addEventListener("click", function () {
+  buttonMedian.remove();
+  buttonBeginner.remove();
+  buttonProfessional.remove()
+  professional();
+});
+
+function beginner() {
   setInterval(createBalloon, 1000);
 }
 
+function median() {
+  setInterval(createBalloon, 500);
+}
+
+function professional() {
+  setInterval(createBalloon, 200);
+}
+
 const balloonsContainer = document.querySelector(".container-balloons");
+const balaoNaTela = document.querySelector('.balaoTela');
 
 function createBalloon() {
   const elementImg = document.createElement("img");
@@ -25,14 +55,29 @@ function createBalloon() {
 
   elementImg.addEventListener("click", function () {
     removeBalloon(this);
-  });
 
+  });
   balloonsContainer.appendChild(elementImg);
+  ++balaoNaTela.innerHTML
+  if (balaoNaTela.innerHTML >= 10) {
+    return document.querySelector('.perdeu').style.display = 'block'
+  }
 }
+    function voltar() {
+      document.querySelector('.btn-voltar').style.display = 'block'
+      function bntVoltar(this.){
+        location.reload(true);
+      }
+    }
 
 function removeBalloon(element) {
   const boomSound = new Audio("./assets/boom.mpeg");
   boomSound.play();
   boomSound.volume = 0.1;
   element.remove();
+  ++contadorDeBalao.innerHTML
+    --balaoNaTela.innerHTML
+  if (contadorDeBalao.innerHTML >= 5) {
+    return document.querySelector('.ganhou').style.display = 'block' && voltar()
+  }
 }
